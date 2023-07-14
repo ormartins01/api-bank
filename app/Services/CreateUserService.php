9d1f4c93d65808cb203c4215f81ebaf1f901464c
Ricardo Martins <hikfm2@gmail.com>
2023-07-13 23:40:21 -0300

    feat: Base project

---
+++ app/Services/CreateUserService.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace App\Services;
+
+class UserCreateService {
+    public function execute(array $data) {
+        
+    }
+}
---
+++ database/migrations/2023_07_14_013328_create_user_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+
+
+
+    public function up(): void
+    {
+        Schema::create('user', function (Blueprint $table) {
+            $table->uuid('id');
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->string('cpf')->unique();
+            $table->string('password');
+            $table->string('type');
+            $table->decimal('balance')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('user');
+    }
+};
